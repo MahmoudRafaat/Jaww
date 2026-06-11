105d08755dd775bf3f54b6f45ef5cf94cb871025
mahmoud raafat <mahmoudrafatt995@gmail.com>
2026-06-11 00:02:42 -0700

    added location service to get the current loaction

--- JawwWeatherApp/Presentation/Home/Components/LocationDeniedView.swift
+++ JawwWeatherApp/Presentation/Home/Components/LocationDeniedView.swift
@@ -5,3 +5,39 @@
 //  Created by Mahmoud Raafat Mustafa on 10/06/2026.
 //
 
+import SwiftUI
+
+struct LocationDeniedView: View {
+    var body: some View {
+        VStack(spacing: 16) {
+            Image(systemName: "location.slash.fill")
+                .font(.system(size: 52))
+                .foregroundColor(.gray)
+            
+            Text("Location Access Denied")
+                .font(.system(size: 20, weight: .semibold))
+                .foregroundColor(.white)
+            
+            Text("Jaww needs your location to show local weather. Please enable it in Settings.")
+                .font(.system(size: 15))
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 32)
+            
+            Button {
+                if let url = URL(string: UIApplication.openSettingsURLString) {
+                    UIApplication.shared.open(url)
+                }
+            } label: {
+                Label("Go to Settings", systemImage: "gear")
+                    .font(.system(size: 16, weight: .medium))
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 24)
+                    .padding(.vertical, 12)
+                    .background(Color.blue)
+                    .cornerRadius(12)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
